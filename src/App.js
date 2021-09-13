ba1050615564b64a157ebceb62b2311e24e01653
Ausitn Reeve <austin.reeve@aggiemail.usu.edu>
2021-09-13 01:52:45 -0600

    added mailchimp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+
+mailchimp.setConfig({
+  apiKey: process.env.MAIL_API,
+  server: process.env.MAIL_SERVER,
+});
 
 function App() {
+
+  async function callPing() {
+    const response = await mailchimp.ping.get();
+    console.log(response);
+  }
+
+  callPing();
+
   return (
     <div className="App">
       <header className="App-header">
